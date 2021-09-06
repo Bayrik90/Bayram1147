124d3cf1583edafe1cc0d3ddb0ecad4fc6eb6ad0
Bayrik90 <90194121+Bayrik90@users.noreply.github.com>
2021-09-06 16:54:44 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main1 {
+    public static void main(String[] args) {
+        float a = 9;
+        float b = 5;
+        Scanner scan = new Scanner(System.in);
+        float TC = scan.nextFloat();
+        System.out.println(a/b*TC+32);
+
+    }
+}
