0389496933da27a391090e8ffb330832a7d0f8dd
Bayrik90 <90194121+Bayrik90@users.noreply.github.com>
2021-09-06 15:47:18 +0300

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String name;
-        int age;
-        String city;
+        String name = "Bayram";
+        int age = 31;
+        String city = "Moscow";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
     }
 }
