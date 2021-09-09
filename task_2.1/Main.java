6a6b60de0af83ab9e8e9427500d3d23e37de114d
Bayrik90 <90194121+Bayrik90@users.noreply.github.com>
2021-09-09 22:13:54 +0300

    Create Main.java

---
+++ task_2.1/Main.java
@@ -0,0 +1,9 @@
+String[]komb={"Мама","Мыла","Раму"};
+        for(int i=0;i< komb.length;i++){
+            for(int j=0;j< komb.length;j++){
+                for(int l=0;l< komb.length;l++){
+                    System.out.println(komb[i]+komb[j]+komb[l]);
+                }
+            }
+
+        }
