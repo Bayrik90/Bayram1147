cc31598d98f084695dea1abc58af520006a5fa53
Bayrik90 <90194121+Bayrik90@users.noreply.github.com>
2021-09-17 20:07:09 +0300

    Create Main.java

---
+++ task_5.3/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <Integer> nums=new ArrayList<>();
+            for(int i=1;i<10;i++){
+                nums.add(i);
+            }
+        System.out.println(nums);
+
+    }
+}
