9ee95a65752ea9b8d71cc133274412e14654975c
Bayrik90 <90194121+Bayrik90@users.noreply.github.com>
2021-09-19 23:40:43 +0300

    Create Main.java

---
+++ task_2.6/Main.java
@@ -0,0 +1,12 @@
+/*
+Задание: Даны переменные a и b. Проверьте, что a делится без остатка на b. Если это так - выведите 'Делится' и результат деления, иначе выведите 'Делится с остатком' и остаток от деления.
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a= scan.nextInt();
+        int b= scan.nextInt();
+        if(a%b==0) System.out.println("Делится без остатка - "+(a/b));
+        else System.out.println("делится с остатком - "+a%b);
+    }
+}
